8c39220ec9a8dea1a865ecd954c76469e180e539
Student <example@example.com>
2019-10-29 14:27:33 -0700

    add some function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
-    
+  $("form#fortuneTeller").submit(function(event){
+    event.preventDefault();
+
+
+    $("input:checkbox[name=unlucky]:checked").each(function(){
+      var unlucky1 = $(this).val();
+      console.log(unlucky1)
+      var total = [];
+      console.log(total);
+
+      $(total).push(unlucky1);
+      console.log(total);
+    });
+    // $("input:checkbox[name=unlucky]:checked").each(function(){
+    //   var unlucky1 = $(this).val();
+    //   $('#fun-responses').append(funTransportationMode + "<br>");
+    // });
+
   });
 });
